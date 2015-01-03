45634e00c33045b9bdac4e2a8eb527384a1bee1d
Alexey Nurgaliev <atnurgaliev@gmail.com>
2015-01-03 18:43:01 +0300

    example documentation

---
+++ res/documented/ui.js
@@ -0,0 +1,57 @@
+
+$(function() {
+    styleTables();
+    responsiveImages();
+    setupMenu();
+    menuCollapserBinding();
+});
+
+function styleTables() {
+    $('.documented-content table').addClass('table');
+}
+
+function responsiveImages() {
+    $('.documented-content img').addClass('img-responsive');
+}
+
+function setupMenu() {
+    var $menuWrapper = $('.documented-menu-wrapper');
+    var $subMenuTitles = $menuWrapper.find('.sub-menu-title');
+
+    var toggleIcon = function(marker) {
+        marker.toggleClass('glyphicon-chevron-right glyphicon-chevron-down');
+    };
+
+    var $subMenuMarkers = $subMenuTitles.find('span.menu-marker');
+
+    //hide sub menus
+    $subMenuMarkers.each(function() {
+        var $this = $(this);
+        toggleIcon($this);
+        $this.parent().siblings('.documented-sub-menu').hide();
+    });
+
+    //show current menu items
+    $('.documented-sub-menu .sub-menu-title.menu-item-current')
+        .children('.menu-marker')
+        .each(function() {
+            var $this = $(this);
+            toggleIcon($this);
+            $this.parent().siblings('.documented-sub-menu').show();
+        });
+
+    //marker click handler
+    $subMenuMarkers.click(function() {
+        var $this = $(this);
+        toggleIcon($this);
+        $this.parent().siblings('.documented-sub-menu').slideToggle();
+    });
+
+    $menuWrapper.show();
+}
+
+function menuCollapserBinding() {
+    $('.documented-menu-collapser').click(function() {
+        $('.documented-menu-collapsible').toggleClass('menu-collapsed');
+    });
+}
